672a815b461fc1331071875d43fe78e297f26193
Bhaskar Bordoloi <bhaskarbordoloi68@gmail.com>
2026-03-08 22:34:14 +0530

    Delete notes/01-basics/03-retrieval-models/boolean-retrieval.py

--- notes/01-basics/03-retrieval-models/boolean-retrieval.py
+++
@@ -1,9 +0,0 @@
-index = {
-    "cat": {1, 2, 5},
-    "dog": {2, 3, 5},
-    "mouse": {3},
-}
-
-# Query: cat AND dog
-result = index["cat"].intersection(index["dog"])
-print(f"Relevant Documents: {result}")
